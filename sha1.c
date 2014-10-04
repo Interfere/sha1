eb7d62eabb66371173f424b7ed49a2d75c038c8a
Alexey Komnin <interfere.work@gmail.com>
2014-10-04 14:33:23 +0400

    group sha1 calc blocks

--- sha1.c
+++ sha1.c
@@ -9,10 +9,35 @@
 #include <stdio.h>
 #include <string.h>
 
-#define F_60_79(M, L, K)    F_20_39((M), (L), (K))
+#define F_60_79         F_20_39
 
 #define SHA1_ONE_ITER(a, b, c, d, e, K, F, X) \
 (e) = _rotl((a), 5) + F((b), (c), (d)) + (e) + (K) + (X),(b) = _rotl((b), 30)
+
+#define SHA1_UPDATE_BUF(W, N) \
+(W)[(N)%16] = _rotl((W)[((N)+13)%16] ^ (W)[((N)+8)%16] ^ (W)[((N)+2)%16] ^ (W)[(N)%16], 1)
+
+#define SHA1_BLOCK(a, b, c, d, e, K, F, W, N) \
+SHA1_ONE_ITER(a, b, c, d, e, K, F, SHA1_UPDATE_BUF(W, (N)));\
+SHA1_ONE_ITER(e, a, b, c, d, K, F, SHA1_UPDATE_BUF(W, (N)+1));\
+SHA1_ONE_ITER(d, e, a, b, c, K, F, SHA1_UPDATE_BUF(W, (N)+2));\
+SHA1_ONE_ITER(c, d, e, a, b, K, F, SHA1_UPDATE_BUF(W, (N)+3));\
+SHA1_ONE_ITER(b, c, d, e, a, K, F, SHA1_UPDATE_BUF(W, (N)+4));\
+SHA1_ONE_ITER(a, b, c, d, e, K, F, SHA1_UPDATE_BUF(W, (N)+5));\
+SHA1_ONE_ITER(e, a, b, c, d, K, F, SHA1_UPDATE_BUF(W, (N)+6));\
+SHA1_ONE_ITER(d, e, a, b, c, K, F, SHA1_UPDATE_BUF(W, (N)+7));\
+SHA1_ONE_ITER(c, d, e, a, b, K, F, SHA1_UPDATE_BUF(W, (N)+8));\
+SHA1_ONE_ITER(b, c, d, e, a, K, F, SHA1_UPDATE_BUF(W, (N)+9));\
+SHA1_ONE_ITER(a, b, c, d, e, K, F, SHA1_UPDATE_BUF(W, (N)+10));\
+SHA1_ONE_ITER(e, a, b, c, d, K, F, SHA1_UPDATE_BUF(W, (N)+11));\
+SHA1_ONE_ITER(d, e, a, b, c, K, F, SHA1_UPDATE_BUF(W, (N)+12));\
+SHA1_ONE_ITER(c, d, e, a, b, K, F, SHA1_UPDATE_BUF(W, (N)+13));\
+SHA1_ONE_ITER(b, c, d, e, a, K, F, SHA1_UPDATE_BUF(W, (N)+14));\
+SHA1_ONE_ITER(a, b, c, d, e, K, F, SHA1_UPDATE_BUF(W, (N)+15));\
+SHA1_ONE_ITER(e, a, b, c, d, K, F, SHA1_UPDATE_BUF(W, (N)));\
+SHA1_ONE_ITER(d, e, a, b, c, K, F, SHA1_UPDATE_BUF(W, (N)+1));\
+SHA1_ONE_ITER(c, d, e, a, b, K, F, SHA1_UPDATE_BUF(W, (N)+2));\
+SHA1_ONE_ITER(b, c, d, e, a, K, F, SHA1_UPDATE_BUF(W, (N)+3))
 
 static inline uint32_t F_00_19(uint32_t x, uint32_t y, uint32_t u)
 {
@@ -63,99 +88,67 @@ static void SHA1_calc(struct SHA_ctx *ctx, uint32_t M[16])
     uint32_t a = ctx->A, b = ctx->B, c = ctx->C, d = ctx->D, e = ctx->E;
     uint32_t W[80];
     
-    for(int t = 0; t < 16; ++t)
-    {
-        W[t] = bswap_32(M[t]);
-    }
+    W[0] = bswap_32(M[0]);
+    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[0]);
+    
+    W[1] = bswap_32(M[1]);
+    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[1]);
+    
+    W[2] = bswap_32(M[2]);
+    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[2]);
+    
+    W[3] = bswap_32(M[3]);
+    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[3]);
+    
+    W[4] = bswap_32(M[4]);
+    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[4]);
+    
+    W[5] = bswap_32(M[5]);
+    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[5]);
+    
+    W[6] = bswap_32(M[6]);
+    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[6]);
+    
+    W[7] = bswap_32(M[7]);
+    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[7]);
+    
+    W[8] = bswap_32(M[8]);
+    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[8]);
+    
+    W[9] = bswap_32(M[9]);
+    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[9]);
+    
+    W[10] = bswap_32(M[10]);
+    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[10]);
+    
+    W[11] = bswap_32(M[11]);
+    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[11]);
+    
+    W[12] = bswap_32(M[12]);
+    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[12]);
+    
+    W[13] = bswap_32(M[13]);
+    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[13]);
+    
+    W[14] = bswap_32(M[14]);
+    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[14]);
+    
+    W[15] = bswap_32(M[15]);
+    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[15]);
     
     for (int t = 16; t < 80; ++t)
     {
         W[t] = _rotl(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1);
     }
     
-    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[0]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[1]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[2]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[3]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[4]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[5]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[6]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[7]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[8]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[9]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[10]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[11]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[12]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[13]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[14]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_00_19, F_00_19, W[15]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, W[16]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, W[17]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, W[18]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, W[19]);
+    SHA1_ONE_ITER(e, a, b, c, d, K_00_19, F_00_19, SHA1_UPDATE_BUF(W, 0));
+    SHA1_ONE_ITER(d, e, a, b, c, K_00_19, F_00_19, SHA1_UPDATE_BUF(W, 1));
+    SHA1_ONE_ITER(c, d, e, a, b, K_00_19, F_00_19, SHA1_UPDATE_BUF(W, 2));
+    SHA1_ONE_ITER(b, c, d, e, a, K_00_19, F_00_19, SHA1_UPDATE_BUF(W, 3));
     
-    SHA1_ONE_ITER(a, b, c, d, e, K_20_39, F_20_39, W[20]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_20_39, F_20_39, W[21]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_20_39, F_20_39, W[22]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_20_39, F_20_39, W[23]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_20_39, F_20_39, W[24]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_20_39, F_20_39, W[25]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_20_39, F_20_39, W[26]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_20_39, F_20_39, W[27]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_20_39, F_20_39, W[28]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_20_39, F_20_39, W[29]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_20_39, F_20_39, W[30]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_20_39, F_20_39, W[31]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_20_39, F_20_39, W[32]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_20_39, F_20_39, W[33]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_20_39, F_20_39, W[34]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_20_39, F_20_39, W[35]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_20_39, F_20_39, W[36]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_20_39, F_20_39, W[37]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_20_39, F_20_39, W[38]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_20_39, F_20_39, W[39]);
-    
-    SHA1_ONE_ITER(a, b, c, d, e, K_40_59, F_40_59, W[40]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_40_59, F_40_59, W[41]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_40_59, F_40_59, W[42]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_40_59, F_40_59, W[43]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_40_59, F_40_59, W[44]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_40_59, F_40_59, W[45]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_40_59, F_40_59, W[46]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_40_59, F_40_59, W[47]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_40_59, F_40_59, W[48]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_40_59, F_40_59, W[49]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_40_59, F_40_59, W[50]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_40_59, F_40_59, W[51]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_40_59, F_40_59, W[52]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_40_59, F_40_59, W[53]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_40_59, F_40_59, W[54]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_40_59, F_40_59, W[55]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_40_59, F_40_59, W[56]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_40_59, F_40_59, W[57]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_40_59, F_40_59, W[58]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_40_59, F_40_59, W[59]);
-    
-    SHA1_ONE_ITER(a, b, c, d, e, K_60_79, F_60_79, W[60]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_60_79, F_60_79, W[61]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_60_79, F_60_79, W[62]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_60_79, F_60_79, W[63]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_60_79, F_60_79, W[64]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_60_79, F_60_79, W[65]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_60_79, F_60_79, W[66]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_60_79, F_60_79, W[67]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_60_79, F_60_79, W[68]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_60_79, F_60_79, W[69]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_60_79, F_60_79, W[70]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_60_79, F_60_79, W[71]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_60_79, F_60_79, W[72]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_60_79, F_60_79, W[73]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_60_79, F_60_79, W[74]);
-    SHA1_ONE_ITER(a, b, c, d, e, K_60_79, F_60_79, W[75]);
-    SHA1_ONE_ITER(e, a, b, c, d, K_60_79, F_60_79, W[76]);
-    SHA1_ONE_ITER(d, e, a, b, c, K_60_79, F_60_79, W[77]);
-    SHA1_ONE_ITER(c, d, e, a, b, K_60_79, F_60_79, W[78]);
-    SHA1_ONE_ITER(b, c, d, e, a, K_60_79, F_60_79, W[79]);
+    SHA1_BLOCK(a, b, c, d, e, K_20_39, F_20_39, W, 4);
+    SHA1_BLOCK(a, b, c, d, e, K_40_59, F_40_59, W, 8);
+    SHA1_BLOCK(a, b, c, d, e, K_60_79, F_60_79, W, 12);
     
     ctx->A += a;
     ctx->B += b;
